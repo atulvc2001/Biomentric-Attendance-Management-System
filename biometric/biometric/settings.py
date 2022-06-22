c812a218e1bdf4e39aaa9941991cdd4733e0ad5e
atvc0202 <atulvc2001@gmail.com>
2022-06-22 23:25:42 +0530

    initialised a test function in views.py

--- biometric/biometric/settings.py
+++ biometric/biometric/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'biometric.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'Student_db',
+        'USER': 'postgres',
+        'PASSWORD': 't0175141c',
+        'HOST': 'localhost'
     }
 }
 
